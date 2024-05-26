39e557ba2b8a719f7d3191c540e6f4174eb07c94
Savvy325 <vannahlyles@yahoo.com>
2024-05-26 18:10:57 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+// import SessionStorageManager from './components/SessionStorageManager'
+  // commented our first part so Login component will work. You can switch between both to see 
+  // both components work
+import Login from "./components/Login"
+
+
+const App = () => {
+  return (
+    <div>
+      {/* <SessionStorageManager/> */}
+      <Login />
+    </div>
+  )
+}
+
+export default App
